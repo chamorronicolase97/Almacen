3b67fa7f95d988cf2d88acbd0b6a4f16433f28b0
Nicolas <chamorronicolase@gmail.com>
2024-07-31 16:31:57 -0300

    Clases Permiso y PermisoGrupo

---
+++ Clases/Sistema/Permiso.cs
@@ -0,0 +1,155 @@
+﻿using Sistema;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Almacen.Clases.Administracion
+{
+    public class Permiso
+    {
+        private int _id;
+        private string _codPermiso;
+        private string _descripcion;
+
+        #region Constantes
+        const string Tabla = "dbo.Permisos";
+        public const string NombreClase = "Permiso";
+        #endregion
+
+        #region Propiedades
+        public int ID { get { return _id; } set { _id = value; } }
+        public string CodPermiso { get { return _codPermiso; } set { _codPermiso = value; } }
+        public string Descripcion { get { return _descripcion; } set { _descripcion = value; } }
+        #endregion
+
+        public Permiso() { }
+        public Permiso(int ID)
+        {
+            this.ID = ID;
+            if (ID != 0) Abrir();
+        }
+        public Permiso(DataRow dr) => CargaDatos(dr);
+
+        public void Abrir()
+        {
+            Conexion cn = new Conexion();
+            string q = @$"Select * from {Tabla} where PermisoID = {ID}";
+
+            DataTable dt = cn.Consultar(q);
+            try
+            {
+
+                if (dt.Rows.Count > 0)
+                {
+                    CargaDatos(dt.Rows[0]);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Error al abrir el objeto {NombreClase}. Clave {ID}", ex);
+            }
+
+        }
+
+
+
+        private void CargaDatos(DataRow dr)
+        {
+            ID = Convert.ToInt32(dr["PermisoID"]);
+            _codPermiso = Convert.ToString(dr["CodPermiso"]);
+            _descripcion = Convert.ToString(dr["Descripcion"]);
+        }
+
+
+        public void Insertar()
+        {
+            Conexion cn = new Conexion();
+            string q = $@"INSERT INTO {Tabla} (CodPermiso, Descripcion)
+                        Values(@CodPermiso, @Descripcion);";
+            SqlCommand cmd = new SqlCommand(q);
+            cmd.Parameters.Add("@CodPermiso", SqlDbType.VarChar, 50).Value = CodPermiso;
+            cmd.Parameters.Add("@Descripcion", SqlDbType.VarChar, 50).Value = Descripcion;
+
+            cn.Ejecutar(cmd);
+
+        }
+
+        public void Modificar()
+        {
+            Conexion cn = new Conexion();
+            string q = $@"UPDATE {Tabla} SET CodPermiso = @CodPermiso, 
+                                             Descripcion = @Descripcion
+                                             WHERE PermisoID = @ID;";
+            SqlCommand cmd = new SqlCommand(q);
+            cmd.Parameters.Add("@CodPermiso", SqlDbType.VarChar, 50).Value = CodPermiso;
+            cmd.Parameters.Add("@Descripcion", SqlDbType.VarChar).Value = Descripcion;
+            cmd.Parameters.Add("@ID", SqlDbType.Int).Value = ID;
+
+            cn.Ejecutar(cmd);
+
+        }
+
+        public void Eliminar()
+        {
+            Conexion cn = new Conexion();
+            string q = $@"DELETE FROM {Tabla} WHERE PermisoID = @ID;";
+            SqlCommand cmd = new SqlCommand(q);
+            cmd.Parameters.Add("@ID", SqlDbType.Int).Value = ID;
+
+            cn.Ejecutar(cmd);
+
+        }
+
+        public static DataTable Listar()
+        {
+            Conexion cn = new Conexion();
+            string q = @$"Select * from {Tabla}";
+            return cn.Consultar(q);
+        }
+
+        public static List<Permiso> ListarPermisos()
+        {
+            DataTable dt = Listar();
+            List<Permiso> lista = new List<Permiso>();
+            foreach (DataRow dr in dt.Rows)
+            {
+                lista.Add(new Permiso(dr));
+            }
+
+            return lista;
+        }
+
+        public static bool ExistePermiso(string CodPermiso)
+        {
+            Conexion cn = new Conexion(); 
+            string q = $@"SELECT * FROM {Tabla}
+            WHERE CodPermiso = @CodPermiso";
+            SqlCommand cmd = new SqlCommand(q);
+            cmd.Parameters.Add("@CodPermiso", SqlDbType.VarChar, 50).Value = CodPermiso;
+            DataTable dt = cn.Consultar(q);
+            
+            return dt.Rows.Count > 0;
+        }
+
+        public static Permiso GetPermiso(string CodPermiso)
+        {
+            Permiso permiso = new Permiso();
+            Conexion cn = new Conexion();
+            string q = $@"SELECT * FROM {Tabla}
+            WHERE CodPermiso = @CodPermiso";
+            SqlCommand cmd = new SqlCommand(q);
+            cmd.Parameters.Add("@CodPermiso", SqlDbType.VarChar, 50).Value = CodPermiso;
+            DataTable dt = cn.Consultar(q);
+
+            if (dt.Rows.Count > 0)
+            {
+                permiso = new Permiso(dt.Rows[1]);
+            }
+                return permiso;
+        }
+    }
+}
---
+++ Clases/Sistema/PermisoGrupo.cs
@@ -0,0 +1,114 @@
+﻿using Sistema;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Almacen.Clases.Administracion
+{
+    public class PermisoGrupo
+    {
+        private Permiso _permiso;
+        private Grupo _grupo;
+
+        #region Constantes
+        const string Tabla = "dbo.PermisosPermisoGrupos";
+        public const string NombreClase = "PermisoGrupo";
+        #endregion
+
+        #region Propiedades
+        public Permiso Permiso { get { return _permiso; } set { _permiso = value; } }
+        public Grupo Grupo { get { return _grupo; }set { _grupo = value; } }
+        #endregion
+
+        public PermisoGrupo() { }
+        public PermisoGrupo(int GrupoID, string CodPermiso)
+        {
+            this.Grupo = new Grupo(GrupoID);
+            this.Permiso = Permiso.GetPermiso(CodPermiso);
+            if (GrupoID != 0 && CodPermiso.Length > 0) Abrir();
+        }
+        public PermisoGrupo(DataRow dr) => CargaDatos(dr);
+
+        public void Abrir()
+        {
+            Conexion cn = new Conexion();
+            string q = @$"Select * from {Tabla} where GrupoID = {Grupo.ID} and CodPermiso = {Permiso.CodPermiso} ";
+
+            DataTable dt = cn.Consultar(q);
+            try
+            {
+
+                if (dt.Rows.Count > 0)
+                {
+                    CargaDatos(dt.Rows[0]);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Error al abrir el objeto {NombreClase}. Clave {Grupo.ID} {Permiso.CodPermiso}", ex);
+            }
+
+        }
+
+
+
+        private void CargaDatos(DataRow dr)
+        {
+            _grupo = null;
+            if (dr["GrupoID"] != DBNull.Value) _grupo = new Grupo(Convert.ToInt32(dr["GrupoID"]));
+
+            _permiso = null;
+            if (dr["CodPermiso"] != DBNull.Value) _permiso = Permiso.GetPermiso(dr["CodPermiso"].ToString()); ;
+        }
+
+
+        public void Insertar()
+        {
+            Conexion cn = new Conexion();
+            string q = $@"INSERT INTO {Tabla} (GrupoID, CodPermiso)
+                        Values(@GrupoID, @CodPermiso);";
+            SqlCommand cmd = new SqlCommand(q);
+            cmd.Parameters.Add("@GrupoID", SqlDbType.Int).Value = Grupo.ID;
+            cmd.Parameters.Add("@CodPermiso", SqlDbType.VarChar).Value = Permiso.CodPermiso;
+
+            cn.Ejecutar(cmd);
+
+        }
+
+
+        public void Eliminar()
+        {
+            Conexion cn = new Conexion();
+            string q = $@"DELETE FROM {Tabla} WHERE GrupoID = @GrupoID and CodPermiso = @CodPermiso ;";
+            SqlCommand cmd = new SqlCommand(q);
+            cmd.Parameters.Add("@GrupoID", SqlDbType.Int).Value = Grupo.ID;
+            cmd.Parameters.Add("@CodPermiso", SqlDbType.Int).Value = Permiso.CodPermiso;
+
+            cn.Ejecutar(cmd);
+
+        }
+
+        public static DataTable Listar()
+        {
+            Conexion cn = new Conexion();
+            string q = @$"Select * from {Tabla}";
+            return cn.Consultar(q);
+        }
+
+        public static List<PermisoGrupo> ListarPermisosPermisoGrupos()
+        {
+            DataTable dt = Listar();
+            List<PermisoGrupo> lista = new List<PermisoGrupo>();
+            foreach (DataRow dr in dt.Rows)
+            {
+                lista.Add(new PermisoGrupo(dr));
+            }
+
+            return lista;
+        }
+    }
+}
