647c1646901747a3e1333c239694e7d14fe03e02
Milton <miltonsarkissian@hotmail.com>
2024-08-04 12:54:44 -0300

    clase pedido, parcial, falta detalle

---
+++ Vistas/Compras/Pedidos/frmABMSPedidos.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace Almacen.Vistas
+{
+    partial class frmABMSPedidos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmABMSPedidos));
+            dgvDatos = new DataGridView();
+            splitContainer1 = new SplitContainer();
+            pnlBotones = new Panel();
+            btnBorrar = new Button();
+            brnModificar = new Button();
+            btnCrear = new Button();
+            txtFiltro = new TextBox();
+            ((System.ComponentModel.ISupportInitialize)dgvDatos).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
+            splitContainer1.Panel1.SuspendLayout();
+            splitContainer1.Panel2.SuspendLayout();
+            splitContainer1.SuspendLayout();
+            pnlBotones.SuspendLayout();
+            SuspendLayout();
+            // 
+            // dgvDatos
+            // 
+            dgvDatos.AllowUserToAddRows = false;
+            dgvDatos.AllowUserToDeleteRows = false;
+            dgvDatos.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            dgvDatos.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvDatos.Location = new Point(-21, 4);
+            dgvDatos.Margin = new Padding(3, 2, 3, 2);
+            dgvDatos.Name = "dgvDatos";
+            dgvDatos.RowHeadersWidth = 51;
+            dgvDatos.RowTemplate.Height = 29;
+            dgvDatos.Size = new Size(602, 295);
+            dgvDatos.TabIndex = 0;
+            // 
+            // splitContainer1
+            // 
+            splitContainer1.Dock = DockStyle.Fill;
+            splitContainer1.Location = new Point(0, 0);
+            splitContainer1.Margin = new Padding(3, 2, 3, 2);
+            splitContainer1.Name = "splitContainer1";
+            splitContainer1.Orientation = Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            splitContainer1.Panel1.Controls.Add(pnlBotones);
+            splitContainer1.Panel1.Controls.Add(txtFiltro);
+            // 
+            // splitContainer1.Panel2
+            // 
+            splitContainer1.Panel2.Controls.Add(dgvDatos);
+            splitContainer1.Size = new Size(584, 361);
+            splitContainer1.SplitterDistance = 63;
+            splitContainer1.SplitterWidth = 3;
+            splitContainer1.TabIndex = 1;
+            // 
+            // pnlBotones
+            // 
+            pnlBotones.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            pnlBotones.Controls.Add(btnBorrar);
+            pnlBotones.Controls.Add(brnModificar);
+            pnlBotones.Controls.Add(btnCrear);
+            pnlBotones.Location = new Point(427, 10);
+            pnlBotones.Name = "pnlBotones";
+            pnlBotones.Size = new Size(151, 45);
+            pnlBotones.TabIndex = 13;
+            // 
+            // btnBorrar
+            // 
+            btnBorrar.Image = (Image)resources.GetObject("btnBorrar.Image");
+            btnBorrar.Location = new Point(103, 4);
+            btnBorrar.Margin = new Padding(3, 2, 3, 2);
+            btnBorrar.Name = "btnBorrar";
+            btnBorrar.Size = new Size(41, 36);
+            btnBorrar.TabIndex = 11;
+            btnBorrar.UseVisualStyleBackColor = true;
+            btnBorrar.Click += btnBorrar_Click;
+            // 
+            // brnModificar
+            // 
+            brnModificar.Image = (Image)resources.GetObject("brnModificar.Image");
+            brnModificar.Location = new Point(56, 4);
+            brnModificar.Margin = new Padding(3, 2, 3, 2);
+            brnModificar.Name = "brnModificar";
+            brnModificar.Size = new Size(41, 36);
+            brnModificar.TabIndex = 10;
+            brnModificar.UseVisualStyleBackColor = true;
+            brnModificar.Click += btnModificar_Click;
+            // 
+            // btnCrear
+            // 
+            btnCrear.Image = (Image)resources.GetObject("btnCrear.Image");
+            btnCrear.Location = new Point(9, 4);
+            btnCrear.Margin = new Padding(3, 2, 3, 2);
+            btnCrear.Name = "btnCrear";
+            btnCrear.Size = new Size(41, 36);
+            btnCrear.TabIndex = 9;
+            btnCrear.UseVisualStyleBackColor = true;
+            btnCrear.Click += btnCrear_Click;
+            // 
+            // txtFiltro
+            // 
+            txtFiltro.Location = new Point(10, 24);
+            txtFiltro.Margin = new Padding(3, 2, 3, 2);
+            txtFiltro.Name = "txtFiltro";
+            txtFiltro.Size = new Size(165, 23);
+            txtFiltro.TabIndex = 3;
+            // 
+            // frmABMSPedidos
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(584, 361);
+            Controls.Add(splitContainer1);
+            Margin = new Padding(3, 2, 3, 2);
+            MinimumSize = new Size(600, 400);
+            Name = "frmABMSPedidos";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Pedidos";
+            Load += frmABMSPedidos_Load;
+            ((System.ComponentModel.ISupportInitialize)dgvDatos).EndInit();
+            splitContainer1.Panel1.ResumeLayout(false);
+            splitContainer1.Panel1.PerformLayout();
+            splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).EndInit();
+            splitContainer1.ResumeLayout(false);
+            pnlBotones.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private DataGridView dgvDatos;
+        private SplitContainer splitContainer1;
+        private TextBox txtFiltro;
+        private Panel pnlBotones;
+        private Button btnBorrar;
+        private Button brnModificar;
+        private Button btnCrear;
+    }
+}
---
+++ Vistas/Compras/Pedidos/frmAMCPedido.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace Almacen.Vistas
+{
+    partial class frmAMCPedido
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnAceptar = new Button();
+            btnCancelar = new Button();
+            txtFechaEntrega = new TextBox();
+            label1 = new Label();
+            label3 = new Label();
+            txtID = new TextBox();
+            SuspendLayout();
+            // 
+            // btnAceptar
+            // 
+            btnAceptar.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            btnAceptar.Location = new Point(175, 113);
+            btnAceptar.Margin = new Padding(3, 2, 3, 2);
+            btnAceptar.Name = "btnAceptar";
+            btnAceptar.Size = new Size(82, 22);
+            btnAceptar.TabIndex = 0;
+            btnAceptar.Text = "Aceptar";
+            btnAceptar.UseVisualStyleBackColor = true;
+            btnAceptar.Click += btnAceptar_Click;
+            // 
+            // btnCancelar
+            // 
+            btnCancelar.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            btnCancelar.Location = new Point(263, 113);
+            btnCancelar.Margin = new Padding(3, 2, 3, 2);
+            btnCancelar.Name = "btnCancelar";
+            btnCancelar.Size = new Size(82, 22);
+            btnCancelar.TabIndex = 1;
+            btnCancelar.Text = "Cancelar";
+            btnCancelar.UseVisualStyleBackColor = true;
+            btnCancelar.Click += btnCancelar_Click;
+            // 
+            // txtFechaEntrega
+            // 
+            txtFechaEntrega.Anchor = AnchorStyles.Bottom;
+            txtFechaEntrega.Location = new Point(97, 56);
+            txtFechaEntrega.Margin = new Padding(3, 2, 3, 2);
+            txtFechaEntrega.Name = "txtFechaEntrega";
+            txtFechaEntrega.Size = new Size(173, 23);
+            txtFechaEntrega.TabIndex = 2;            // 
+            // label1
+            // 
+            label1.Anchor = AnchorStyles.Bottom;
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 59);
+            label1.Name = "label1";
+            label1.Size = new Size(81, 15);
+            label1.TabIndex = 4;
+            label1.Text = "Fecha Entrega";
+            // 
+            // label3
+            // 
+            label3.Anchor = AnchorStyles.Bottom;
+            label3.AutoSize = true;
+            label3.Location = new Point(71, 34);
+            label3.Name = "label3";
+            label3.Size = new Size(18, 15);
+            label3.TabIndex = 6;
+            label3.Text = "ID";
+            // 
+            // txtID
+            // 
+            txtID.Anchor = AnchorStyles.Bottom;
+            txtID.Enabled = false;
+            txtID.Location = new Point(97, 32);
+            txtID.Margin = new Padding(3, 2, 3, 2);
+            txtID.Name = "txtID";
+            txtID.Size = new Size(66, 23);
+            txtID.TabIndex = 7;
+            txtID.TextChanged += this.txtID_TextChanged;
+            // 
+            // frmAMCPedido
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(355, 144);
+            Controls.Add(txtFechaEntrega);
+            Controls.Add(txtID);
+            Controls.Add(label3);
+            Controls.Add(label1);
+            Controls.Add(btnCancelar);
+            Controls.Add(btnAceptar);
+            FormBorderStyle = FormBorderStyle.FixedDialog;
+            Margin = new Padding(3, 2, 3, 2);
+            MaximizeBox = false;
+            MdiChildrenMinimizedAnchorBottom = false;
+            MinimizeBox = false;
+            Name = "frmAMCPedido";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Pedido";
+            Load += frmAMCPedido_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnAceptar;
+        private Button btnCancelar;
+        private TextBox txtFechaEntrega;
+        private Label label1;
+        private Label label3;
+        private TextBox txtID;
+    }
+}
