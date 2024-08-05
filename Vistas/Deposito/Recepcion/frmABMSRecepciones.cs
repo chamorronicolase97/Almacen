993ac605f8da0d63755625252626ca061bc22106
Nicolas <chamorronicolase@gmail.com>
2024-08-05 09:20:52 -0300

    Pantallas de Recepción

---
+++ Vistas/Deposito/Recepcion/frmABMSRecepciones.cs
@@ -0,0 +1,79 @@
+﻿using Almacen.Clases;
+using Almacen.Clases.Compra;
+using Sistema;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Almacen.Vistas
+{
+    public partial class frmABMSRecepciones : Form
+    {
+
+        public frmABMSRecepciones()
+        {
+            InitializeComponent();
+
+        }
+
+        private void frmABMSPedidos_Load(object sender, EventArgs e)
+
+        {
+            dgvDatos.DataSource = Pedido.Listar();
+        }
+
+        private void CargarGrilla()
+        {
+            dgvDatos.DataSource = Pedido.Listar();
+        }
+
+        private void btnCrear_Click(object sender, EventArgs e)
+        {
+            frmAMCRecepcion f = new frmAMCRecepcion();
+            f.Clase = new Recepcion(0);
+            f.ShowDialog();
+            if (f.DialogResult == DialogResult.OK) CargarGrilla();
+        }
+
+        private void btnBorrar_Click(object sender, EventArgs e)
+        {
+            if (dgvDatos.CurrentRow == null) return;
+
+            Pedido Clase = new Pedido(Convert.ToInt32(dgvDatos.CurrentRow.Cells["NroPedido"].Value));
+
+            DialogResult = MessageBox.Show("Desea eliminar el Pedido " + Clase.ID + "?", "Eliminar", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            if (DialogResult == DialogResult.No) return;
+
+            if (Clase.TieneRecepcion())
+            {
+                frmMostrarMensaje.MostrarMensaje($"{Pedido.NombreClase}", "El " + Pedido.NombreClase + " posee una recepción, no puede eliminarse.");
+                return;
+            }
+            Clase.Eliminar();
+
+            frmMostrarMensaje.MostrarMensaje($"{Pedido.NombreClase}", "Baja de " + Pedido.NombreClase + " exitosa.");
+
+            CargarGrilla();
+
+        }
+
+        private void btnModificar_Click(object sender, EventArgs e)
+        {
+            if (dgvDatos.CurrentRow == null) return;
+
+            Recepcion Clase = new Recepcion(Convert.ToInt32(dgvDatos.CurrentRow.Cells["RecepcionID"].Value));
+
+            frmAMCRecepcion f = new frmAMCRecepcion();
+            f.Clase = Clase;
+            f.Modificacion = true;
+            f.ShowDialog();
+            if (f.DialogResult == DialogResult.OK) CargarGrilla();
+        }
+    }
+}
---
+++ Vistas/Deposito/Recepcion/frmAMCRecepcion.cs
@@ -0,0 +1,88 @@
+﻿using Almacen.Clases;
+using Almacen.Clases.Compra;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Almacen.Vistas
+{
+    public partial class frmAMCRecepcion : Form
+    {
+        public Recepcion Clase { get; set; }
+
+        public bool Modificacion { get; set; } = false;
+
+        public frmAMCRecepcion()
+        {
+            InitializeComponent();
+
+        }
+
+        private void frmAMCPedido_Load(object sender, EventArgs e)
+        {
+            if (Modificacion == true)
+            {
+                CargarGrillaDetalles();
+                txtNroPedido.Text = Clase.ID.ToString();
+                dtpFechaEntrega.Value = Clase.FechaEntrega;
+            }
+            else
+            {
+
+            }
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+            if (!Validar()) return;
+
+            Clase.FechaEntrega = dtpFechaEntrega.Value;
+
+            if (Modificacion)
+            {
+                Clase.Modificar();
+                this.DialogResult = DialogResult.OK;
+            }
+            else
+            {
+                Clase.Insertar();
+                this.DialogResult = DialogResult.OK;
+            }
+        }
+
+        private bool Validar()
+        {
+            if (dtpFechaEntrega == null)
+            {
+                frmMostrarMensaje.MostrarMensaje("Pedido", "Debe definir una fecha para el Pedido");
+                return false;
+            }
+
+            return true;
+        }
+
+        private void btnAsignar_Click(object sender, EventArgs e)
+        {
+            frmAMCDetalleRecepcion f = new frmAMCDetalleRecepcion();
+            f.Show();
+            CargarGrillaDetalles();
+        }
+
+        private void CargarGrillaDetalles()
+        {
+            dgvDetalles.DataSource = DetallePedido.Listar();
+        }
+
+    }
+}
