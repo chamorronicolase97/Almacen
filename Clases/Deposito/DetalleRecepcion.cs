7a621bf58cd5d192a65ac5a56e3ea56464ea3d2a
Nicolas <chamorronicolase@gmail.com>
2024-08-05 09:11:12 -0300

    Clases Recepcion y DetalleRecepcion

---
+++ Clases/Deposito/DetalleRecepcion.cs
@@ -0,0 +1,150 @@
+﻿using Almacen.Clases.Administracion;
+using Sistema;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Almacen.Clases.Compra
+{
+    public class DetalleRecepcion
+    {
+        private Recepcion _recepcion;
+        private Producto producto;
+        private Proveedor proveedor;
+        private int _cantidad;
+        private decimal _costoUnitario;
+        public DateTime _fechaRecepcion;
+
+        #region Constantes
+        const string Tabla = "dbo.DetallesRecepciones";
+        public const string NombreClase = "DetalleRecepcion";
+        #endregion
+
+        #region Propiedades
+        public Recepcion Recepcion { get { return _recepcion; } set { _recepcion = value; } }
+        public Producto Producto { get { return producto; } set { producto = value; } }
+        public Proveedor Proveedor { get { return proveedor; } set { proveedor = value; } }
+        public int Cantidad { get { return _cantidad; } set { _cantidad = value; } }
+        public decimal CostoUnitario { get { return _costoUnitario; } set { _costoUnitario = value; } }
+        public DateTime FechaRecepcion { get { return _fechaRecepcion; }set { _fechaRecepcion = value; } }
+        #endregion
+
+        public DetalleRecepcion() { }
+        //public DetalleRecepcion(int RecepcionID, int productoID, int proveedorID)
+        //{
+        //    this.Recepcion = RecepcionID;
+        //    if (ID != 0) Abrir();
+        //}
+        public DetalleRecepcion(DataRow dr) => CargaDatos(dr);
+
+        public void Abrir()
+        {
+            Conexion cn = new Conexion();
+            string q = @$"Select * from {Tabla} where RecepcionID = @RecepcionID
+                                                and ProductoID = @productoID
+                                                and ProveedorID = @proveedorID";
+
+            SqlCommand cmd = new SqlCommand(q);
+            cmd.Parameters.Add("@RecepcionID", SqlDbType.Int).Value = Recepcion.ID;
+            cmd.Parameters.Add("@productoID", SqlDbType.Int).Value = Producto.ID;
+            cmd.Parameters.Add("@proveedorID", SqlDbType.Int).Value = Proveedor.ID;
+
+            DataTable dt = cn.Consultar(q);
+            try
+            {
+
+                if (dt.Rows.Count > 0)
+                {
+                    CargaDatos(dt.Rows[0]);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Error al abrir el objeto {NombreClase}. Clave {Recepcion.ID + Producto.ID + Proveedor.ID}", ex);
+            }
+
+        }
+
+        private void CargaDatos(DataRow dr)
+        {
+            Recepcion = new Recepcion(Convert.ToInt32(dr["RecepcionID"]));
+            producto = new Producto(Convert.ToInt32(dr["ProductoID"]));
+            proveedor = new Proveedor(Convert.ToInt32(dr["ProveedorID"]));
+            _cantidad = Convert.ToInt32(dr["Cantidad"]);
+            _costoUnitario = Convert.ToDecimal(dr["CostoUnitario"]);
+            _fechaRecepcion = Convert.ToDateTime(dr["FechaRecepcion"]);
+        }
+
+        public void Insertar()
+        {
+            Conexion cn = new Conexion();
+            string q = $@"INSERT INTO {Tabla} (RecepcionID, ProductoID, ProveedorID, Cantidad, CostoUnitario, FechaRecepcion)
+                        Values(@RecepcionID, @ProductoID, @ProveedorID, @cantidad, @costoUnitario, @fechaRecepcion);";
+            SqlCommand cmd = new SqlCommand(q);
+            cmd.Parameters.Add("@RecepcionID", SqlDbType.Int).Value = Recepcion.ID;
+            cmd.Parameters.Add("@ProductoID", SqlDbType.Int).Value = Producto.ID;
+            cmd.Parameters.Add("@ProveedorID", SqlDbType.Int).Value = Proveedor.ID;
+            cmd.Parameters.Add("@cantidad", SqlDbType.Int).Value = Cantidad;
+            cmd.Parameters.Add("@costoUnitario", SqlDbType.Decimal).Value = CostoUnitario;
+            cmd.Parameters.Add("@fechaRecepcion", SqlDbType.DateTime).Value = FechaRecepcion;
+            cn.Ejecutar(cmd);
+        }
+
+        public void Modificar()
+        {
+            Conexion cn = new Conexion();
+            string q = $@"UPDATE {Tabla} SET Cantidad = @cantidad,
+                                             CostoUnitario = @costoUnitario
+                                             WHERE RecepcionID = @RecepcionID
+                                             and ProductoID = @productoID
+                                             and ProveedorID = @proveedorID;";
+            SqlCommand cmd = new SqlCommand(q);
+            cmd.Parameters.Add("@cantidad", SqlDbType.Int).Value = Cantidad;
+            cmd.Parameters.Add("@costoUnitario", SqlDbType.Decimal).Value = CostoUnitario;
+            cmd.Parameters.Add("@RecepcionID", SqlDbType.Int).Value = Recepcion.ID;
+            cmd.Parameters.Add("@productoID", SqlDbType.Int).Value = Producto.ID;
+            cmd.Parameters.Add("@proveedorID", SqlDbType.Int).Value = Proveedor.ID;
+
+            cn.Ejecutar(cmd);
+        }
+
+        public void Eliminar()
+        {
+            Conexion cn = new Conexion();
+            string q = $@"DELETE FROM {Tabla} WHERE RecepcionID = @RecepcionID
+                                              and ProductoID = @productoID
+                                              and ProveedorID = @proveedorID";
+            SqlCommand cmd = new SqlCommand(q);
+            cmd.Parameters.Add("@RecepcionID", SqlDbType.Int).Value = Recepcion.ID;
+            cmd.Parameters.Add("@productoID", SqlDbType.Int).Value = Producto.ID;
+            cmd.Parameters.Add("@proveedorID", SqlDbType.Int).Value = Proveedor.ID;
+
+            cn.Ejecutar(cmd);
+
+        }
+
+        public static DataTable Listar()
+        {
+            Conexion cn = new Conexion();
+            string q = @$"Select * from {Tabla}";
+            return cn.Consultar(q);
+        }
+
+        public static List<DetalleRecepcion> ListarDetallesRecepciones()
+        {
+            DataTable dt = Listar();
+            List<DetalleRecepcion> lista = new List<DetalleRecepcion>();
+            foreach (DataRow dr in dt.Rows)
+            {
+                lista.Add(new DetalleRecepcion(dr));
+            }
+
+            return lista;
+        }
+
+    }
+}
--- Clases/Deposito/Recepcion.cs
+++ Clases/Deposito/Recepcion.cs
@@ -9,33 +9,35 @@ using System.Threading.Tasks;
 
 namespace Almacen.Clases.Compra
 {
-    public class Pedido
+    public class Recepcion
     {
         private int _id;
+        private Pedido? _pedido;
         private DateTime _fechaEntrega;
 
         #region Constantes
-        const string Tabla = "dbo.Pedidos";
-        public const string NombreClase = "Pedido";
+        const string Tabla = "dbo.Recepcions";
+        public const string NombreClase = "Recepcion";
         #endregion
 
         #region Propiedades
         public int ID { get { return _id; } set { _id = value; } }
+        public Pedido? Pedido { get { return _pedido; } set { _pedido = value; } }
         public DateTime FechaEntrega { get { return _fechaEntrega; } set { _fechaEntrega = value; } }
         #endregion
 
-        public Pedido() { }
-        public Pedido(int ID)
+        public Recepcion() { }
+        public Recepcion(int ID)
         {
             this.ID = ID;
             if (ID != 0) Abrir();
         }
-        public Pedido(DataRow dr) => CargaDatos(dr);
+        public Recepcion(DataRow dr) => CargaDatos(dr);
 
         public void Abrir()
         {
             Conexion cn = new Conexion();
-            string q = @$"Select * from {Tabla} where NroPedido = {ID}";
+            string q = @$"Select * from {Tabla} where RecepcionID = {ID}";
 
             DataTable dt = cn.Consultar(q);
             try
@@ -55,16 +57,22 @@ namespace Almacen.Clases.Compra
 
         private void CargaDatos(DataRow dr)
         {
-            ID = Convert.ToInt32(dr["NroPedido"]);
+            ID = Convert.ToInt32(dr["RecepcionID"]);
+
+            _pedido = null;
+
+            if (dr["NroPedido"] != DBNull.Value) { _pedido = new Pedido(Convert.ToInt32(dr["NroPedido"])); }
+
             _fechaEntrega = Convert.ToDateTime(dr["FechaEntrega"]);
         }
 
         public void Insertar()
         {
             Conexion cn = new Conexion();
-            string q = $@"INSERT INTO {Tabla} (FechaEntrega)
-                        Values(@fecha);";
+            string q = $@"INSERT INTO {Tabla} (NroPedido, FechaEntrega)
+                        Values(@NroPedido, @fecha);";
             SqlCommand cmd = new SqlCommand(q);
+            cmd.Parameters.Add("@NroPedido", SqlDbType.Int).Value = Pedido.ID;
             cmd.Parameters.Add("@fecha", SqlDbType.DateTime).Value = FechaEntrega;
 
             cn.Ejecutar(cmd);
@@ -73,9 +81,11 @@ namespace Almacen.Clases.Compra
         public void Modificar()
         {
             Conexion cn = new Conexion();
-            string q = $@"UPDATE {Tabla} SET FechaEntrega = @fecha, 
-                                             WHERE NroPedido = @ID;";
+            string q = $@"UPDATE {Tabla} SET NroPedido = @NroPedido;
+                                             FechaEntrega = @fecha 
+                                             WHERE RecepcionID = @ID;";
             SqlCommand cmd = new SqlCommand(q);
+            cmd.Parameters.Add("@NroPedido", SqlDbType.Int).Value = Pedido.ID;
             cmd.Parameters.Add("@fecha", SqlDbType.DateTime).Value = FechaEntrega;
             cmd.Parameters.Add("@ID", SqlDbType.Int).Value = ID;
 
@@ -85,7 +95,7 @@ namespace Almacen.Clases.Compra
         public void Eliminar()
         {
             Conexion cn = new Conexion();
-            string q = $@"DELETE FROM {Tabla} WHERE NroPedido = @ID;";
+            string q = $@"DELETE FROM {Tabla} WHERE RecepcionID = @ID;";
             SqlCommand cmd = new SqlCommand(q);
             cmd.Parameters.Add("@ID", SqlDbType.Int).Value = ID;
 
@@ -100,13 +110,13 @@ namespace Almacen.Clases.Compra
             return cn.Consultar(q);
         }
 
-        public static List<Pedido> ListarPedidos()
+        public static List<Recepcion> ListarRecepcions()
         {
             DataTable dt = Listar();
-            List<Pedido> lista = new List<Pedido>();
+            List<Recepcion> lista = new List<Recepcion>();
             foreach (DataRow dr in dt.Rows)
             {
-                lista.Add(new Pedido(dr));
+                lista.Add(new Recepcion(dr));
             }
 
             return lista;
@@ -115,7 +125,7 @@ namespace Almacen.Clases.Compra
         public bool TieneRecepcion()
         {
             Conexion cn = new Conexion();
-            string q = $@"SELECT TOP 1 * FROM dbo.Recepciones WHERE NroPedido = @ID";
+            string q = $@"SELECT TOP 1 * FROM dbo.Recepciones WHERE RecepcionID = @ID";
             SqlCommand cmd = new SqlCommand(q);
             cmd.Parameters.Add("@ID", SqlDbType.Int).Value = ID;
 
