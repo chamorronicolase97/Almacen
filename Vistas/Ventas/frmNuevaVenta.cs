f13a87593c57c995cfe959d3e1ba14f4b0c69fce
Nicolas <chamorronicolase@gmail.com>
2024-09-10 23:33:12 -0300

    Modificacion Pantalla Venta, filtros

--- Vistas/Ventas/frmNuevaVenta.cs
+++ Vistas/Ventas/frmNuevaVenta.cs
@@ -21,6 +21,7 @@ namespace Almacen.Vistas.Ventas
 
         private Cliente _cliente;
         private Producto _producto;
+        private List<Categoria> categorias;
         private List<DetalleVenta> _detalleVenta;
         private decimal _subtotal;
         private decimal _descuento;
@@ -28,6 +29,7 @@ namespace Almacen.Vistas.Ventas
         private Venta _venta;
         private DetalleVenta _detalle;
         private Usuario _usuario;
+        private BindingSource bindingSource;
 
         public Cliente Cliente { get { return _cliente; } set { _cliente = value; } }
         public Usuario Usuario { get { return _usuario; } set { _usuario = value; } }
@@ -35,10 +37,22 @@ namespace Almacen.Vistas.Ventas
         public frmNuevaVenta()
         {
             InitializeComponent();
+            bindingSource = new BindingSource();
         }
 
         private void frmNuevaVenta_Load(object sender, EventArgs e)
         {
+            categorias = Categoria.ListarCategorias();
+
+            Categoria cat = new Categoria();
+            cat.CategoriaID = 0;
+            cat.Descripcion = "Categoria";
+            categorias.Insert(0, cat);
+
+            cmbCategoria.DisplayMember = "Descripcion";
+            cmbCategoria.ValueMember = "CategoriaID";
+            cmbCategoria.DataSource = categorias;
+
             txtCliente.Text = _cliente.Denominacion;
             txtOperador.Text = _usuario.NombreApellido;
             _detalleVenta = new List<DetalleVenta>();
@@ -50,6 +64,7 @@ namespace Almacen.Vistas.Ventas
 
             _venta.Insertar();
 
+            CargarProductos();
             CargarDetalleVenta();
 
         }
@@ -98,9 +113,9 @@ namespace Almacen.Vistas.Ventas
 
             dgvDatos.Columns["DetalleVentaID"].Visible = false;
 
-           CalcularSubTotal();
-           CalcularDescuento();
-           CalcularTotal();
+            CalcularSubTotal();
+            CalcularDescuento();
+            CalcularTotal();
 
         }
 
@@ -169,7 +184,7 @@ namespace Almacen.Vistas.Ventas
 
             DetalleVenta Clase = new DetalleVenta(Convert.ToInt32(dgvDatos.CurrentRow.Cells["DetalleVentaID"].Value), _venta.ID);
 
-             
+
             if (MessageBox.Show("Desea eliminar el detalle " + Clase.ID + "?", "Eliminar", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes)
             {
                 Clase.Eliminar();
@@ -177,9 +192,79 @@ namespace Almacen.Vistas.Ventas
                 frmMostrarMensaje.MostrarMensaje($"{DetalleVenta.NombreClase}", "Baja de " + DetalleVenta.NombreClase + " exitosa.");
             }
 
-            _detalleVenta.RemoveAll(x => x.ID == Clase.ID && x.Venta.ID == Clase.Venta.ID);            
+            _detalleVenta.RemoveAll(x => x.ID == Clase.ID && x.Venta.ID == Clase.Venta.ID);
 
             CargarDetalleVenta();
+        }
+
+        private void dgvProductos_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dgvProductos.CurrentRow == null) return;
+
+            _producto = new Producto(Convert.ToInt32(dgvProductos.CurrentRow.Cells["ProductoID"].Value));
+
+
+
+            InputCantidad inputCantidad = new InputCantidad();
+            inputCantidad.ShowDialog();
+            if (inputCantidad.DialogResult != DialogResult.OK) return;
+            int cantidad = Convert.ToInt32(inputCantidad.Cantidad);
+
+            _detalle = new DetalleVenta();
+            _detalle.Cantidad = cantidad;
+            _detalle.Venta = _venta;
+            _detalle.Producto = _producto;
+
+            //calcular SubTotal
+            _detalle.SubTotal = _detalle.CalcularSubTotal;
+
+            _detalle.Insertar();
+
+            _detalleVenta.Add(_detalle);
+
+            CargarDetalleVenta();
+        }
+
+        private void CargarProductos()
+        {
+            string query = $@"SELECT ProductoID, Descripcion, dbo.PrecioVenta(ProductoID) 'Precio Venta', CategoriaID FROM dbo.Productos WHERE Costo IS NOT NULL";
+
+            SqlCommand cmd = new SqlCommand(query);
+
+            Conexion cn = new Conexion();
+
+            DataTable dtDato = cn.Consultar(cmd);
+
+            dgvProductos.DataSource = dtDato;
+
+            dgvProductos.Columns["CategoriaID"].Visible = false;
+            dgvProductos.Columns["ProductoID"].Visible = false;
+
+            bindingSource.DataSource = dtDato;
+
+            dgvDatos.DataSource = bindingSource;
+
+        }
+
+        private void txtFiltro_TextChanged(object sender, EventArgs e)
+        {
+            string filtro = txtFiltro.Text.Trim().ToLower();
+            if (string.IsNullOrEmpty(filtro))
+            {
+                bindingSource.RemoveFilter();
+            }
+            else
+            {
+                bindingSource.Filter = $@"Descripcion LIKE '%{filtro}%'";
+            }
+        }
+
+        private void cmbCategoria_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cmbCategoria.SelectedIndex == 0) return;
+
+            Categoria categoria = (Categoria)cmbCategoria.SelectedItem;
+            bindingSource.Filter = $@"CategoriaID = {categoria.CategoriaID}";
         }
     }
 }
